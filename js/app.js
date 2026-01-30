404a27837b61f91932a62aa552ec426668034d83
maeda-takumi <jiangqiantian54@gmail.com>
2026-01-30 12:20:48 +0900

    04

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const ghostButtons = document.querySelectorAll('.ghost-button');
-
-ghostButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    button.classList.toggle('is-active');
-  });
-});
